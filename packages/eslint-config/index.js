81f78d59dd268663138f411589c7993053fb7e2e
chaesunbak <coanstjd0422@gmail.com>
2025-12-15 12:42:20 +0900

    chore : eslint 공통 패키지에 import 정렬 플러그인 & 정렬 규칙 추가

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,9 +1,12 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
+const { builtinModules } = require("node:module");
 
 module.exports = {
   extends: [
     "prettier", // Prettier와 충돌하는 ESLint 규칙 비활성화
   ],
+
+  plugins: ["simple-import-sort"],
 
   ignorePatterns: [
     // Build outputs
@@ -39,6 +42,30 @@ module.exports = {
 
     // Console 사용 규칙 (warn, error, info는 허용)
     "no-console": ["warn", { allow: ["warn", "error", "info"] }],
+
+    // Import 정렬
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [
+          // Node.js builtins
+          [`^(node:)?(${builtinModules.join("|")})(/.*|$)`],
+          // External packages
+          ["^@?\\w"],
+          // Internal packages & aliases
+          ["^(@repo|@|~)(/.*|$)"],
+          // Side effect imports
+          ["^\\u0000"],
+          // Parent imports
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          // Relative imports
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+          // Style imports
+          ["^.+\\.s?css$"],
+        ],
+      },
+    ],
+    "simple-import-sort/exports": "error",
 
     // Rushstack의 엄격한 규칙들 완화
     "@typescript-eslint/explicit-member-accessibility": "off", // public/private 강제 해제
