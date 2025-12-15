1a674374547e3aab1c1db9c7cc6f016444e1b4ee
chaesunbak <coanstjd0422@gmail.com>
2025-12-15 13:03:53 +0900

    fix : 클라이언트 eslint 플러그인에서 next제거

--- apps/client/.eslintrc.js
+++ apps/client/.eslintrc.js
@@ -1,15 +1,29 @@
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  extends: [
-    "@rushstack/eslint-config/profile/web-app",
-    "@org/eslint-config",
-    "next/core-web-vitals",
-    "next/typescript",
-  ],
+  extends: ['@rushstack/eslint-config/profile/web-app', '@org/eslint-config'],
   parserOptions: {
-    project: "./tsconfig.json",
+    project: './tsconfig.json',
     tsconfigRootDir: __dirname,
+  },
+  rules: {
+    // React/Next.js 특화 규칙
+    // '@typescript-eslint/naming-convention': [
+    //   'error',
+    //   {
+    //     selector: 'variable',
+    //     format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+    //     leadingUnderscore: 'allow',
+    //   },
+    //   {
+    //     selector: 'function',
+    //     format: ['camelCase', 'PascalCase'],
+    //   },
+    //   {
+    //     selector: 'typeLike',
+    //     format: ['PascalCase'],
+    //   },
+    // ],
   },
   root: true,
 };
