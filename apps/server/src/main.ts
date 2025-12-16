d5c917fb8e988890b3554feea1e641515ed1374f
ryurain0309 <lyw3837@soongsil.ac.kr>
2025-12-16 17:28:51 +0900

    feat: SDK 모듈 관련 파일 생성

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // SDK 호출을 위해 개발 환경에서 모든 출처에 대해 CORS 허용
+  // 실제 운영 환경에서는 SDK 도메인에 대한 동적 CORS 설정 필요
+  app.enableCors('*');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
