0105c994b4a2fd33b0475586ef7f8af01bd55124
ryurain0309 <lyw3837@soongsil.ac.kr>
2025-12-16 18:24:56 +0900

    feat: cookie-parser 미들웨어 추가 및 Cookies 데코레이터 생성
    client에서 전송되는 gzip 데이터가 프레임워크에 의해 자동으로 압축 해제되는 것을 방지하기 위해, 로그 업로드 경로를 BodyParser 미들웨어 적용 대상에서 제외

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,8 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { json, urlencoded } from 'express';
+
+import { SdkModule } from '#domain/sdk/sdk.module';
 
 @Module({
-  imports: [],
+  imports: [SdkModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  // Content-Encoding의 의해 gzip 압축해제가 안되도록 명시적 설정
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(json(), urlencoded({ extended: true }))
+      .exclude({ path: 'sdk/replay_logs', method: RequestMethod.POST })
+      .forRoutes('*');
+  }
+}
