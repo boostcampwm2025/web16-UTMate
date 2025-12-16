8908e61cf6bf8a545a070606779e6a3fce9401cd
chaesunbak <coanstjd0422@gmail.com>
2025-12-16 17:57:02 +0900

    feat : rrweb event 전송 스크립트
    - 최초 lwt 쿼리파라미터 확인 후
    - 5초간격마다 이벤트 전송
    - sendBeacon API로 페이지 이탈시 전송

---
+++ apps/sdk/src/constants.ts
@@ -0,0 +1,2 @@
+export const EVENT_SEND_INTERVAL = 5 * 1000;
+export const EVENT_SEND_URL = 'http://localhost:3000/sdk/replay_logs';
--- apps/sdk/src/index.ts
+++ apps/sdk/src/index.ts
@@ -1,13 +1,59 @@
-export class MySDK {
-  private name: string;
+import { record } from '@rrweb/record';
+import type { eventWithTime } from '@rrweb/types';
 
-  constructor(name: string) {
-    this.name = name;
-    console.log(`SDK Initialized: ${name}`);
+import { EVENT_SEND_INTERVAL, EVENT_SEND_URL } from './constants';
+
+(() => {
+  if (!checkIsLwt()) {
+    return;
   }
 
-  public sayHello() {
-    console.log(`Hello from ${this.name}!`);
-    return `Hello from ${this.name}!`;
+  let eventQueue: eventWithTime[] = [];
+
+  record({
+    emit(event) {
+      eventQueue.push(event);
+    },
+  });
+
+  // save events every 5 seconds
+  setInterval(async () => {
+    await sendEvents();
+  }, EVENT_SEND_INTERVAL);
+
+  // 세션 종료시 이벤트 전송
+  // https://developer.mozilla.org/ko/docs/Web/API/Navigator/sendBeacon 참고
+  document.addEventListener('visibilitychange', () => {
+    if (document.visibilityState === 'hidden') {
+      navigator.sendBeacon(EVENT_SEND_URL, JSON.stringify({ events: eventQueue }));
+    }
+  });
+
+  /**
+   * lwt 파라미터가 true인지 여부를 확인합니다.
+   * @returns lwt 파라미터가 true인지 여부
+   */
+  function checkIsLwt(): boolean {
+    const searchParams = new URLSearchParams(window.location.search);
+    const isLwt = searchParams.get('lwt') === 'true';
+    return isLwt;
   }
-}
+
+  async function sendEvents() {
+    if (eventQueue.length === 0) return;
+
+    const events = [...eventQueue];
+    eventQueue = []; // 큐 초기화
+
+    const body = JSON.stringify({ events });
+
+    await fetch(EVENT_SEND_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        withCredentials: 'true',
+      },
+      body,
+    });
+  }
+})();
