f19815fedef0f4eb019bbd34fe62e18c87a6532a
chaesunbak <coanstjd0422@gmail.com>
2025-12-16 19:06:02 +0900

    feat : rrweb데이터 gzip으로 전달

--- apps/sdk/src/index.ts
+++ apps/sdk/src/index.ts
@@ -1,5 +1,6 @@
 import { record } from '@rrweb/record';
 import type { eventWithTime } from '@rrweb/types';
+import pako from 'pako';
 
 import { EVENT_SEND_INTERVAL, EVENT_SEND_URL } from './constants';
 
@@ -24,8 +25,11 @@ import { EVENT_SEND_INTERVAL, EVENT_SEND_URL } from './constants';
   // 세션 종료시 이벤트 전송
   // https://developer.mozilla.org/ko/docs/Web/API/Navigator/sendBeacon 참고
   document.addEventListener('visibilitychange', () => {
-    if (document.visibilityState === 'hidden') {
-      navigator.sendBeacon(EVENT_SEND_URL, JSON.stringify({ events: eventQueue }));
+    if (document.visibilityState === 'hidden' && eventQueue.length > 0) {
+      const jsonString = JSON.stringify({ events: eventQueue });
+      const compressed = pako.gzip(jsonString);
+      const blob = new Blob([compressed], { type: 'application/gzip' });
+      navigator.sendBeacon(EVENT_SEND_URL, blob);
     }
   });
 
@@ -45,15 +49,17 @@ import { EVENT_SEND_INTERVAL, EVENT_SEND_URL } from './constants';
     const events = [...eventQueue];
     eventQueue = []; // 큐 초기화
 
-    const body = JSON.stringify({ events });
+    const jsonString = JSON.stringify({ events });
+    const compressed = pako.gzip(jsonString);
 
     await fetch(EVENT_SEND_URL, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
-        withCredentials: 'true',
+        'Content-Type': 'application/gzip',
+        credentials: 'include', //서로 다른 도메인(크로스 도메인)에 요청을 보낼 때 요청에 credential 정보(쿠키 등)를 담아서 보낼 지를 결정하는 항목
+        // 다음 헤더가 들어가면 서버가 자동으로 해제함으로 포함하면 안됨 'Content-Encoding': 'gzip',
       },
-      body,
+      body: compressed,
     });
   }
 })();
