e9ba8dd3e2200a5f923fd8942d7fc18323c18905
ryurain0309 <lyw3837@soongsil.ac.kr>
2025-12-18 18:09:24 +0900

    fix: StorageController의 S3 파일 제공 엔드포인트 수정 및 모듈 모듈 추가

--- apps/server/src/common/storage/storage.controller.ts
+++ apps/server/src/common/storage/storage.controller.ts
@@ -1,17 +1,19 @@
 import fs from 'fs';
 
-import { Controller, Get, Param, Res, StreamableFile } from '@nestjs/common';
+import { Controller, Get, Logger, Param, Res, StreamableFile } from '@nestjs/common';
 import type { Response } from 'express';
 
 @Controller('/storage')
 export class StorageController {
   // S3 흉내용 엔드포인트
-  @Get('/s3/:key(.*)')
+  @Get('/s3/*key')
   getFile(@Param('key') key: string, @Res({ passthrough: true }) res: Response) {
     res.set({
       'Content-Type': 'application/gzip',
     });
-    const fileStream = fs.createReadStream(key);
+    const filePath = Array.isArray(key) ? key.join('/') : key;
+    Logger.log(`${Date.now()} - Serving file from S3 mock: ${filePath}`, 'StorageController');
+    const fileStream = fs.createReadStream(filePath);
     return new StreamableFile(fileStream);
   }
 }
