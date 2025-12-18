41b9f9153d6689fa393db686438ce5a13fd12cd4
chaesunbak <coanstjd0422@gmail.com>
2025-12-18 17:38:28 +0900

    feat : 버튼 공통 컴포넌트

---
+++ apps/client/src/components/common/Button.tsx
@@ -0,0 +1,21 @@
+import { cn } from '@/utils';
+
+type ButtonVariant = 'outline';
+
+export function Button({
+  variant = 'outline',
+  className,
+  children,
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement> & { variant?: ButtonVariant }) {
+  return (
+    <button
+      {...props}
+      className={cn('rounded-md p-2 cursor-pointer', className, {
+        'border border-gray-300 bg-white text-gray-500': variant === 'outline',
+      })}
+    >
+      {children}
+    </button>
+  );
+}
