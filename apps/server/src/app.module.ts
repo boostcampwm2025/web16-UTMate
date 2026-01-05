c4ea060c371383118786aae8d1a413adf1c7ce48
RyuRain0309 <lyw3837@soongsil.ac.kr>
2026-01-05 21:18:27 +0900

    feat: redis mysql 도커 구성 및 Nestjs 연동

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,5 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
-import { json, urlencoded } from 'express';
+import { CacheModule } from '@nestjs/cache-manager';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { redisStore } from 'cache-manager-redis-store';
+import * as Joi from 'joi';
 
 import { StorageModule } from '#common/storage/storage.module';
 import { MissionResultModule } from '#domain/mission-result/mission-result.module';
@@ -7,6 +11,60 @@ import { SdkModule } from '#domain/sdk/sdk.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '../../.env',
+      validationSchema: Joi.object({
+        // Server
+        NODE_ENV: Joi.string().valid('development', 'production').default('development'),
+        SERVER_PORT: Joi.number().default(8080),
+
+        // Database
+        DATABASE_HOST: Joi.string().default('localhost'),
+        DATABASE_PORT: Joi.number().default(3306),
+        DATABASE_USER: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+
+        // Redis
+        REDIS_HOST: Joi.string().default('localhost'),
+        REDIS_PORT: Joi.number().default(6379),
+
+        // Client
+        CLIENT_URL: Joi.string().uri().default('http://localhost:3000'),
+      }),
+      validationOptions: {
+        abortEarly: false,
+        stripUnknown: true,
+      },
+    }),
+
+    // DB (MySQL)
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DATABASE_HOST')!,
+        port: config.get<number>('DATABASE_PORT')!,
+        username: config.get<string>('DATABASE_USER')!,
+        password: config.get<string>('DATABASE_PASSWORD')!,
+        database: config.get<string>('DATABASE_NAME')!,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: config.get<string>('NODE_ENV')! !== 'production',
+      }),
+    }),
+
+    // Cache (Redis)
+    CacheModule.registerAsync({
+      isGlobal: true,
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        store: redisStore as unknown,
+        host: config.get<string>('REDIS_HOST')!,
+        port: config.get<number>('REDIS_PORT')!,
+      }),
+    }),
+
     // common modules
     StorageModule,
 
@@ -14,15 +72,5 @@ import { SdkModule } from '#domain/sdk/sdk.module';
     MissionResultModule,
     SdkModule,
   ],
-  controllers: [],
-  providers: [],
 })
-export class AppModule implements NestModule {
-  // Content-Encoding의 의해 gzip 압축해제가 안되도록 명시적 설정
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(json(), urlencoded({ extended: true }))
-      .exclude({ path: 'sdk/replay_logs', method: RequestMethod.POST })
-      .forRoutes('*');
-  }
-}
+export class AppModule {}
