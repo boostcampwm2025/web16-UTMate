136a12ae9f627341682d59ebae28f2485cacb193
jammin94 <kedee2001@naver.com>
2026-01-08 15:24:18 +0900

    feat : 마이페이지 구현

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,13 +1,17 @@
 import { Hero } from '@/features/(landing)/components/Hero';
 import { Features } from '@/features/(landing)/components/Features';
 import { CTASection } from '@/features/(landing)/components/CTASection';
+import { LandingNavBar } from '@/widgets/navigation/LandingNavBar';
 
 export default function LandingPage() {
   return (
-    <main className="min-h-screen">
-      <Hero />
-      <Features />
-      <CTASection />
-    </main>
+    <>
+      <LandingNavBar />
+      <main className="min-h-screen">
+        <Hero />
+        <Features />
+        <CTASection />
+      </main>
+    </>
   );
 }
