f0f9bc0c90a1b126584a8b217c6a478b9505f05b
chaesunbak <coanstjd0422@gmail.com>
2026-01-08 20:30:33 +0900

    feat : 로그인한 유저만 테스트 개별페이지 접속가능

--- apps/client/src/app/(test-detail)/tests/[id]/page.tsx
+++ apps/client/src/app/(test-detail)/tests/[id]/page.tsx
@@ -1,7 +1,8 @@
-import { notFound } from 'next/navigation';
+import { notFound, redirect } from 'next/navigation';
 
 import { getTestById } from '@/features/(test-detail)/api';
 import { TestForm } from '@/features/(test-detail)/components/TestForm';
+import { ApiError } from '@/shared/constants/api';
 
 export default async function TestDetailPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
@@ -11,10 +12,14 @@ export default async function TestDetailPage({ params }: { params: Promise<{ id:
 
     return <TestForm initialData={initialData} />;
   } catch (error) {
-    //TODO : 예외 처리 제대로 하기
-    //TODO: 로그인 한 유저만 접속가능하게 처리(proxy에서 처리하거나 여기서 처리)
-    if (error instanceof Error && error.message === 'Test not found') {
-      notFound();
+    if (error instanceof ApiError) {
+      if (error.code === 404) {
+        notFound();
+      } else if (error.code === 401) {
+        redirect('/login');
+      } else {
+        throw error;
+      }
     }
 
     throw error;
--- apps/client/src/features/(test-detail)/api/index.ts
+++ apps/client/src/features/(test-detail)/api/index.ts
@@ -3,6 +3,7 @@ import { cookies } from 'next/headers';
 import { SERVER_BASE_URL } from '@/shared/constants/api';
 import type { TestDetail } from '@/features/(test-manage)/types';
 import type { ApiErrorResponse } from '@/shared/types/api';
+import { ApiError } from '@/shared/constants/api';
 
 export const getTestById = async (id: string): Promise<TestDetail> => {
   const cookieStore = await cookies();
@@ -18,7 +19,7 @@ export const getTestById = async (id: string): Promise<TestDetail> => {
 
   if (!response.ok) {
     const error = (await response.json()) as ApiErrorResponse;
-    throw new Error(error.message || '테스트를 불러오는데 실패했습니다.');
+    throw new ApiError(error.message || '테스트를 불러오는데 실패했습니다.', error.statusCode);
   }
   return response.json();
 };
