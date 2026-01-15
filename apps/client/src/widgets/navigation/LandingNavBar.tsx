2bea9890c899107fd737cfcca01716d92d338c9d
chaesunbak <coanstjd0422@gmail.com>
2026-01-15 16:31:38 +0900

    fix : LandingNavBar 컴포넌트 서버 컴포넌트로 변경
    - useRouter 대신 Link 내비게이션

--- apps/client/src/widgets/navigation/LandingNavBar.tsx
+++ apps/client/src/widgets/navigation/LandingNavBar.tsx
@@ -1,8 +1,5 @@
-'use client';
-
 import Link from 'next/link';
 import { Button } from '@/shared/components/ui/button';
-import { useRouter } from 'next/navigation'
 
 /**
  * LandingNavBar - 랜딩 페이지 전용 네비게이션 바
@@ -14,15 +11,7 @@ import { useRouter } from 'next/navigation'
  * 로그인되지 않은 사용자를 위한 심플한 네비게이션
  */
 
-
 export function LandingNavBar() {
-
-  const router = useRouter();
-
-  const handleLogin = () => {
-    router.push('/login');
-  };
-
   return (
     <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
       <div className="flex h-16 items-center justify-between px-4 lg:px-6">
@@ -35,9 +24,9 @@ export function LandingNavBar() {
         </Link>
 
         {/* 로그인 버튼 */}
-        <Button onClick={handleLogin} size="lg">
-          로그인
-        </Button>
+        <Link href="/login">
+          <Button size="lg">로그인</Button>
+        </Link>
       </div>
     </header>
   );
