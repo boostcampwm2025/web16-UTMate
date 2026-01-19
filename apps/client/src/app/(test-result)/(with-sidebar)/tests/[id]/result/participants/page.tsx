be9829641bbc785b25d9add266ec5151101f13e8
chaesunbak <coanstjd0422@gmail.com>
2026-01-19 23:23:26 +0900

    chore : TODO 주석 추가

--- apps/client/src/app/(test-result)/(with-sidebar)/tests/[id]/result/participants/page.tsx
+++ apps/client/src/app/(test-result)/(with-sidebar)/tests/[id]/result/participants/page.tsx
@@ -4,6 +4,9 @@ import { TestParticipantsResults } from '@/features/(test-result)/components/Tes
 export default async function ParticipantsResultsPage({ params }: { params: Promise<{ id: string }> }) {
   const { id: testId } = await params;
 
+  //TODO: 404 처리
+  //TODO: 401 처리
+
   return (
     <QueryBoundary>
       <TestParticipantsResults testId={testId} />
