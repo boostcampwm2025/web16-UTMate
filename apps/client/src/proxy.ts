1c00b29761100efbcc73690460b8ac031ad2222c
chaesunbak <coanstjd0422@gmail.com>
2026-01-21 22:30:59 +0900

    feat : 로그인이 필요한 페이지 proxy에서 리다이렉트

---
+++ apps/client/src/proxy.ts
@@ -0,0 +1,24 @@
+import { NextResponse, NextRequest } from 'next/server'
+
+import { getCurrentUseronServer } from '@/features/(auth)/apis/server';
+import type { User } from '@/features/(auth)/types';
+
+export async function proxy(request: NextRequest) {
+  let user: User | null = null;
+  try {
+    user = await getCurrentUseronServer();
+  } catch (error) {
+    user = null;
+  }
+
+  if (!user) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  return NextResponse.next();
+}
+ 
+export const config = {
+  //해당 경로에 접근할 때만 프록시가 실행됨
+  matcher: ['/workspace', '/tests/:path*', '/profile'], // `/participate/:testId`는 제외
+}
