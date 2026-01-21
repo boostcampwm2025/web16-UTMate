bde967d20b2b06ae7eaacf09edf1feb7c4c6e5aa
RyuRain0309 <lyw3837@soongsil.ac.kr>
2026-01-22 02:10:47 +0900

    test: 분석 모듈에 버그가 없는 지 확인하는 테스트 코드 작성

---
+++ apps/server/src/domain/analyzer/analyzer.service.spec.ts
@@ -0,0 +1,39 @@
+import fs from 'fs';
+import path from 'path';
+
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { AnalyzerService } from './analyzer.service';
+
+describe('SdkService', () => {
+  let service: AnalyzerService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [AnalyzerService],
+    }).compile();
+
+    service = module.get<AnalyzerService>(AnalyzerService);
+  });
+
+  it('서비스가 정의되어 있어야 한다', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('로그 분석', () => {
+    it('정상적으로 로그 분석을 완료해야한다.', async () => {
+      const logsBuffer = fs.readFileSync(
+        path.join(__dirname, '../../../uploads/replay_log/sample-log.jsonl'),
+      );
+
+      const analysisResult = service.analyze(logsBuffer);
+
+      expect(analysisResult).toBeDefined();
+      expect(analysisResult.duration).toBeDefined();
+      expect(analysisResult.timeToFirstInteraction).toBeDefined();
+      expect(analysisResult.idleTime).toBeDefined();
+      expect(analysisResult.rageClickCount).toBeDefined();
+      expect(analysisResult.mouseThrashingCount).toBeDefined();
+    });
+  });
+});
