425c796a0e4ee7eab5ac6dc13bb11bc69ee59611
chaesunbak <coanstjd0422@gmail.com>
2026-01-22 14:16:08 +0900

    feat : 테스트 결과 메인페이지 레이아웃 수정

--- apps/client/src/app/(test-result)/(with-sidebar)/tests/[id]/result/page.tsx
+++ apps/client/src/app/(test-result)/(with-sidebar)/tests/[id]/result/page.tsx
@@ -14,21 +14,24 @@ export default async function TestResultSummaryPage({
   //TODO: 401 처리
 
   return (
-    <div className="space-y-4">
+    <div className="flex flex-col h-full gap-6">
       {/* 테스트 기본 정보 */}
       <QueryBoundary>
         <TestBasicInfo testId={testId} />
       </QueryBoundary>
 
-      {/* 미션별 성공률 */}
-      <QueryBoundary>
-        <TestMissionsSummary testId={testId} />
-      </QueryBoundary>
+      {/* 하단 2열 레이아웃: 미션 통계 / 주요 피드백 */}
+      <div className="grid grid-cols-1 lg:grid-cols-[3fr_2fr] gap-6 flex-1 min-h-0">
+        {/* 미션 통계 */}
+        <QueryBoundary>
+          <TestMissionsSummary testId={testId} />
+        </QueryBoundary>
 
-      {/* 주요 피드백 */}
-      <QueryBoundary>
-        <TestMainFeedback testId={testId} />
-      </QueryBoundary>
+        {/* 주요 피드백 */}
+        <QueryBoundary>
+          <TestMainFeedback testId={testId} />
+        </QueryBoundary>
+      </div>
     </div>
   );
 }
