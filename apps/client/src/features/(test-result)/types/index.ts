4b0c5d88452bb7a287c7b1d8ea874b368445919d
chaesunbak <coanstjd0422@gmail.com>
2026-01-27 15:35:11 +0900

    feat : 미션결과 모킹 API 업데이트 & 로그 API 모킹 추가

--- apps/client/src/features/(test-result)/types/index.ts
+++ apps/client/src/features/(test-result)/types/index.ts
@@ -1,23 +1,4 @@
 import { TestStatus } from '@/features/(test-manage)/types';
-
-type MissionResultStatus = 'PENDING' | 'COMPLETED' | 'FAILED' | 'SKIPPED';
-
-interface MissionResult {
-  id: number;
-  participantId: string;
-  missionId: string;
-  status: MissionResultStatus;
-  duration: number;
-  feedback: string | null;
-  createdAt: string;
-  updatedAt: string;
-}
-
-export interface SimpleMissionResult extends Pick<
-  MissionResult,
-  'id' | 'participantId' | 'missionId'
-> {}
-
 export interface TestResultSummary {
   id: number;
   title: string;
@@ -28,8 +9,8 @@ export interface TestResultSummary {
   totalParticipants: number;
 }
 
-// 참여자 결과 관련 타입 추가
-export type ParticipantMissionStatus = 'PENDING' | 'SUCCESS' | 'FAILED' | 'IN_PROGRESS';
+export type MissionResultStatus = 'PENDING' | 'SUCCESS' | 'FAILED' | 'IN_PROGRESS' | 'DROP';
+export type ParticipantMissionStatus = MissionResultStatus;
 
 export interface ParticipantMissionResult {
   missionResultId: string;
