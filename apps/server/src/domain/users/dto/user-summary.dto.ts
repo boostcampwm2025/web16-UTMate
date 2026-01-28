815fa1f6fe34859145032b00dbe291836e3c002a
jammin94 <kedee2001@naver.com>
2026-01-28 19:27:48 +0900

    feat : user-summary에 user email도 추가

--- apps/server/src/domain/users/dto/user-summary.dto.ts
+++ apps/server/src/domain/users/dto/user-summary.dto.ts
@@ -5,12 +5,17 @@ export class UserSummaryDto {
   role: string;
   username: string;
   avatarUrl: string;
+  email?: string;
 
   static fromUserEntity(user: User): UserSummaryDto {
     const dto = new UserSummaryDto();
     dto.publicId = user.publicId;
     dto.username = user.username;
     dto.avatarUrl = user.avatarUrl;
+    // email이 있으면 포함
+    if (user.email) {
+      dto.email = user.email;
+    }
     return dto;
   }
 
