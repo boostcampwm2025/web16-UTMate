ee5bd9562898151cddccc51c1fde2dbeef2d8a89
chaesunbak <coanstjd0422@gmail.com>
2026-02-03 22:27:21 +0900

    refactor : SERVER_URL 환경변수 필요없게 리팩토링
    - 경로를 환경변수로 받는 것이 아니라 프로덕션/개발환경에 따라 다르게 설정하도록 변경

--- apps/sdk/src/constants.ts
+++ apps/sdk/src/constants.ts
@@ -1,5 +1,7 @@
 export const EVENT_SEND_INTERVAL = 5 * 1000;
-export const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:8080/api';
+
+export const SERVER_URL =
+  import.meta.env.MODE === 'production' ? 'https://utmate.me/api' : 'http://localhost:8080/api';
 
 export const RECORDER_SCRIPT_URL =
   import.meta.env.MODE === 'production'
